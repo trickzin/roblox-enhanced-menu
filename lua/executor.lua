63a489097a9e0c1ff0bf4d70385b85575e9338be
0xc <91226824+trickzin@users.noreply.github.com>
2025-05-26 21:55:53 -0300

    executor.lua

--- lua/executor.lua
+++ lua/executor.lua
@@ -1,7 +1,7 @@
 -- Criado por: trickzqw
 -- Email: sintofaltadela@gmail.com(não existe)
 
-local RobloxMenu = loadstring(game:HttpGet("https://raw.githubusercontent.com/trickzqw/roblox-enhanced-menu/main/lua/menu.lua"))()
+local RobloxMenu = loadstring(game:HttpGet("https://raw.githubusercontent.com/trickzin/roblox-enhanced-menu/refs/heads/main/lua/menu.lua"))()
 
 local menu = RobloxMenu.new()
 menu:Initialize()
